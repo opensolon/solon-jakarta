d5f6a991faed12fa68b3368fcfaddf7a253e84c0
noear <noear@live.cn>
2025-09-27 18:56:57 +0800

    添加 solon-server-undertow-jakarta, solon-server-undertow-add-jsp-jakarta 插件

---
+++ solon-jakarta-projects/solon-server/solon-server-undertow-jakarta/src/main/java/org/noear/solon/server/undertow/http/UtHttpContextServletHandler.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2017-2025 noear.org and authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.noear.solon.server.undertow.http;
+
+import org.noear.solon.server.ServerProps;
+import org.noear.solon.web.servlet.SolonServletHandler;
+import org.noear.solon.server.undertow.integration.UndertowPlugin;
+import org.noear.solon.core.handle.Context;
+
+//Servlet模式
+public class UtHttpContextServletHandler extends SolonServletHandler {
+    @Override
+    protected void preHandle(Context ctx) {
+        if (ServerProps.output_meta) {
+            ctx.headerSet("Solon-Server", UndertowPlugin.solon_server_ver());
+        }
+    }
+}
