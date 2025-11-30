a9e0183a5afc811905f0b905f5055a53f748bd83
xls-web <zhanjiangxuwen@126.com>
2025-11-30 12:09:48 +0800

    尝试为tomcat适配jsp支持

--- solon-jakarta-projects/solon-server/solon-server-tomcat-jakarta/src/main/java/org/noear/solon/server/tomcat/TomcatServerJsp.java
+++ solon-jakarta-projects/solon-server/solon-server-tomcat-jakarta/src/main/java/org/noear/solon/server/tomcat/TomcatServerJsp.java
@@ -15,13 +15,27 @@
  */
 package org.noear.solon.server.tomcat;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.catalina.Context;
 import org.apache.catalina.Wrapper;
+import org.apache.catalina.startup.Tomcat;
 import org.apache.jasper.servlet.JasperInitializer;
-import org.apache.jasper.servlet.JspServlet;
+import org.noear.solon.Solon;
+import org.noear.solon.Utils;
+import org.noear.solon.core.runtime.NativeDetector;
+import org.noear.solon.core.util.ResourceUtil;
 import org.noear.solon.server.prop.impl.HttpServerProps;
+import org.noear.solon.server.tomcat.jsp.JspTldLocator;
 
-import java.io.IOException;
+import jakarta.servlet.descriptor.JspConfigDescriptor;
+import jakarta.servlet.descriptor.TaglibDescriptor;
 
 /**
  * @author noear 2025/11/29 created
@@ -34,28 +48,92 @@ public class TomcatServerJsp extends TomcatServer {
     @Override
     protected void initContext() throws IOException {
         Context ctx = getContext();
-
+        
         //jsp
         try {
-            addJspServlet(ctx);
-
-            JasperInitializer jasperInstance = new JasperInitializer();
-            ctx.addServletContainerInitializer(jasperInstance, null);
+        	String resourcePath = getResourceRoot();
+        	if(Utils.isBlank(resourcePath)) {
+        		resourcePath = getClass().getClassLoader().getResource("").getPath();        		
+        	}
+            ctx.setDocBase(resourcePath);
+        	addJspSupport(ctx);
+        	addTldSupport(ctx);
         } catch (Exception e) {
             log.debug(e.getMessage(), e);
         }
     }
 
-    private void addJspServlet(Context context) {
-        Wrapper jspServlet = context.createWrapper();
-        jspServlet.setName("jsp");
-        jspServlet.setServlet(new JspServlet());
+    
+    private void addJspSupport(Context context) throws IOException {
+        // 注册JSP Servlet（Tomcat需要显式注册JspServlet）
+        Wrapper jspServlet = Tomcat.addServlet(context, "jsp", "org.apache.jasper.servlet.JspServlet");
         jspServlet.addInitParameter("fork", "false");
         jspServlet.addInitParameter("xpoweredBy", "false");
-        jspServlet.setLoadOnStartup(3);
-
-        context.addChild(jspServlet);
+//        jspServlet.addInitParameter("development", "true"); // 开发模式，便于调试
+        
+        // 设置JSP文件映射
         context.addServletMappingDecoded("*.jsp", "jsp");
         context.addServletMappingDecoded("*.jspx", "jsp");
+        
+        JasperInitializer jasperInstance = new JasperInitializer();
+        context.addServletContainerInitializer(jasperInstance, null);
     }
+    
+    private void addTldSupport(Context ctx) throws IOException {
+        // 扫描TLD文件并注册
+        Map<String, TaglibDescriptor> tagLibInfos = JspTldLocator.createTldInfos("WEB-INF", "templates");
+        
+        if (tagLibInfos.size()>0) {
+        	List<TaglibDescriptor> taglibs = new ArrayList<>(tagLibInfos.values());
+            JspConfigDescriptor jspConfigDescriptor = new org.apache.tomcat.util.descriptor.web.JspConfigDescriptorImpl(
+                new ArrayList<>(), taglibs);
+            ctx.setJspConfigDescriptor(jspConfigDescriptor);
+//            ctx.getServletContext().setAttribute("jakarta.servlet.context.tldScan", true);
+        }
+    }
+    
+    private String getResourceRoot() throws FileNotFoundException {
+        URL rootURL = getRootPath();
+        if (rootURL == null) {
+            if (NativeDetector.inNativeImage()) {
+                return "";
+            }
+
+            throw new FileNotFoundException("Unable to find root");
+        }
+
+        String resURL = rootURL.toString();
+
+        if (Solon.cfg().isDebugMode() && (resURL.startsWith("jar:") == false)) {
+            int endIndex = resURL.indexOf("target");
+            return resURL.substring(0, endIndex) + "src/main/resources/";
+        }
+
+        return "";
+    }
+
+    private URL getRootPath() {
+        URL root = ResourceUtil.getResource("/");
+        if (root != null) {
+            return root;
+        }
+        try {
+            URL temp = ResourceUtil.getResource("");
+            if (temp == null) {
+                return null;
+            }
+
+            String path = temp.toString();
+            if (path.startsWith("jar:")) {
+                int endIndex = path.indexOf("!");
+                path = path.substring(0, endIndex + 1) + "/";
+            } else {
+                return null;
+            }
+            return new URL(path);
+        } catch (MalformedURLException e) {
+            return null;
+        }
+    }
+    
 }
