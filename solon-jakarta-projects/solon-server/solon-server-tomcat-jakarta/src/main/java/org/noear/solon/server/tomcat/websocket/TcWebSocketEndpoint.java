168ee82ee354a1eeff31e1bde5794e2c2d869043
noear <noear@live.cn>
2025-12-03 11:04:16 +0800

    优化注释

--- solon-jakarta-projects/solon-server/solon-server-tomcat-jakarta/src/main/java/org/noear/solon/server/tomcat/websocket/TcWebSocketEndpoint.java
+++ solon-jakarta-projects/solon-server/solon-server-tomcat-jakarta/src/main/java/org/noear/solon/server/tomcat/websocket/TcWebSocketEndpoint.java
@@ -1,14 +1,24 @@
+/*
+ * Copyright 2017-2025 noear.org and authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.noear.solon.server.tomcat.websocket;
 
-import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.util.concurrent.Future;
 
 import org.noear.solon.net.websocket.WebSocket;
-import org.noear.solon.net.websocket.WebSocketBase;
 import org.noear.solon.net.websocket.WebSocketRouter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import jakarta.websocket.CloseReason;
 import jakarta.websocket.Endpoint;
@@ -19,6 +29,8 @@ import jakarta.websocket.Session;
 /**
  * Tomcat WebSocket端点实现
  *
+ * @author 小xu中年
+ * @since 3.7
  */
 public class TcWebSocketEndpoint extends Endpoint {
     private final String SESSION_KEY = "session";
@@ -52,7 +64,7 @@ public class TcWebSocketEndpoint extends Endpoint {
     }
 
     private static class TextMessageHandler implements MessageHandler.Whole<String> {
-        private WebSocket socket;
+        private final WebSocket socket;
 
         public TextMessageHandler(WebSocket socket) {
             this.socket = socket;
@@ -69,7 +81,7 @@ public class TcWebSocketEndpoint extends Endpoint {
     }
 
     private static class BufferMessageHandler implements MessageHandler.Whole<ByteBuffer> {
-        private WebSocket socket;
+        private final WebSocket socket;
 
         public BufferMessageHandler(WebSocket socket) {
             this.socket = socket;
@@ -81,82 +93,6 @@ public class TcWebSocketEndpoint extends Endpoint {
                 webSocketRouter.getListener().onMessage(socket, s);
             } catch (Throwable e) {
                 webSocketRouter.getListener().onError(socket, e);
-            }
-        }
-    }
-
-    private static class WebSocketImpl extends WebSocketBase {
-        private static final Logger log = LoggerFactory.getLogger(WebSocketImpl.class);
-        private Session real;
-
-        public WebSocketImpl(Session real) {
-            this.real = real;
-            this.init(real.getRequestURI());
-        }
-
-        @Override
-        public boolean isValid() {
-            return isClosed() == false && real.isOpen();
-        }
-
-        @Override
-        public boolean isSecure() {
-            return real.isSecure();
-        }
-
-        @Override
-        public InetSocketAddress remoteAddress() {
-            // Tomcat的Session不直接提供远程地址，这里返回null
-            return null;
-        }
-
-        @Override
-        public InetSocketAddress localAddress() {
-            // Tomcat的Session不直接提供本地地址，这里返回null
-            return null;
-        }
-
-        @Override
-        public long getIdleTimeout() {
-            return real.getMaxIdleTimeout();
-        }
-
-        @Override
-        public void setIdleTimeout(long idleTimeout) {
-            real.setMaxIdleTimeout(idleTimeout);
-        }
-
-        @Override
-        public Future<Void> send(String text) {
-            return real.getAsyncRemote().sendText(text);
-        }
-
-        @Override
-        public Future<Void> send(ByteBuffer binary) {
-            return real.getAsyncRemote().sendBinary(binary);
-        }
-
-        @Override
-        public void close() {
-            super.close();
-            try {
-                real.close();
-            } catch (Throwable ignore) {
-                if (log.isDebugEnabled()) {
-                    log.debug("Close failure: {}", ignore.getMessage());
-                }
-            }
-        }
-
-        @Override
-        public void close(int code, String reason) {
-            super.close(code, reason);
-            try {
-                real.close(new CloseReason(CloseReason.CloseCodes.getCloseCode(code), reason));
-            } catch (Throwable ignore) {
-                if (log.isDebugEnabled()) {
-                    log.debug("Close failure: {}", ignore.getMessage());
-                }
             }
         }
     }
--- solon-jakarta-projects/solon-server/solon-server-tomcat-jakarta/src/main/java/org/noear/solon/server/tomcat/websocket/TcWebSocketManager.java
+++ solon-jakarta-projects/solon-server/solon-server-tomcat-jakarta/src/main/java/org/noear/solon/server/tomcat/websocket/TcWebSocketManager.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017-2025 noear.org and authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.noear.solon.server.tomcat.websocket;
 
 import java.util.Collection;
@@ -13,9 +28,9 @@ import jakarta.websocket.server.ServerEndpointConfig;
 
 /**
  * Tomcat WebSocket 管理器
- * 
- * @author noear
- * @since 3.7.3
+ *
+ * @author 小xu中年
+ * @since 3.7
  */
 public class TcWebSocketManager {
     private static final Logger log = LoggerFactory.getLogger(TcWebSocketManager.class);
